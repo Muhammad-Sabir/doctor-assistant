162faa1af77d351b67828b7ec3d12827d21e5f6b
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-09-24 15:17:24 +0500

    Implement route protection for homepage

--- doctor-assistance-fe/src/pages/home/HomePage.jsx
+++ doctor-assistance-fe/src/pages/home/HomePage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Header from '@/components/home/Header'
 import HeroSection from '@/components/home/HeroSection'
@@ -8,8 +9,20 @@ import TopDoctorsSection from '@/components/home/TopDoctorsSection'
 import MobileAppBanner from '@/components/home/MobileAppBanner'
 import FaqSection from '@/components/home/FaqSection'
 import Footer from '@/components/home/Footer'
+import { getAuthStatus } from '@/utils/auth';
 
 export default function HomePage() {
+  
+  const navigate = useNavigate();
+
+    useEffect(() => {
+        const { isAuthenticated, user } = getAuthStatus();
+
+        if (isAuthenticated) {
+            navigate(`/${user.role}`);
+        }
+    }, [navigate]);
+
   return (
     <div>
       <Header/>
