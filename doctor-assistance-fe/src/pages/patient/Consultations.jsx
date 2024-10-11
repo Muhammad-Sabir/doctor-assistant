500c78c5c509aa5cde3b96f6656acafc995eae38
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-10-11 12:51:57 +0500

    Integrate TopDoctorsSection On Homepage with Backend

--- doctor-assistance-fe/src/pages/patient/Consultations.jsx
+++ doctor-assistance-fe/src/pages/patient/Consultations.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Consultations() {
   return (
-    <div>
-      This is all consultation Page
+    <div className='px-2 pb-4'>
+      <h2 className="text-md font-medium text-primary mb-4">My Consultation History:</h2>
     </div>
   )
 }
