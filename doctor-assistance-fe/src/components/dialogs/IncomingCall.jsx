ee9443f761e8ef6f4e4ca24b39309ef294453cda
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-10-18 21:49:57 +0500

    Implement auto-open for Incoming Call dialog

--- doctor-assistance-fe/src/components/dialogs/IncomingCall.jsx
+++ doctor-assistance-fe/src/components/dialogs/IncomingCall.jsx
@@ -1,34 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import RingingIcon from '@/components/shared/RingingIcon';
 
 export default function IncomingCall() {
-
+    
     const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(true); 
 
-    const handleDecline = (e) => {
-        console.log('Call Declined')
+    const handleDecline = () => {
+        console.log('Call Declined');
+        setIsOpen(false);
     };
 
-    const handleAccept = (e) => {
-        console.log('Call Accepted')
-        navigate('/patient/consultation/video-call')
+    const handleAccept = () => {
+        console.log('Call Accepted');
+        setIsOpen(false); 
+        navigate('/patient/consultation/video-call');
     };
 
     return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant='secondary' className='ml-3'>Video Call</Button>
-            </DialogTrigger>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
                     <DialogTitle>
                         <span>
-                            <h1 className="mb-7 text-center ">Incoming Video Call</h1>
+                            <h1 className="mb-7 text-center">Incoming Video Call</h1>
                             <RingingIcon />
                         </span>
                     </DialogTitle>
@@ -42,9 +40,8 @@ export default function IncomingCall() {
                 <DialogFooter>
                     <Button type="button" variant='secondary' onClick={handleDecline}>Decline</Button>
                     <Button type="button" onClick={handleAccept}>Accept</Button>
-
                 </DialogFooter>
             </DialogContent>
         </Dialog>
-    )
+    );
 }
--- doctor-assistance-fe/src/layouts/DashboardLayout.jsx
+++ doctor-assistance-fe/src/layouts/DashboardLayout.jsx
@@ -1,8 +1,13 @@
-import Sidebar from '@/components/dashboard/Sidebar.jsx'
-import Header from '@/components/dashboard/Header.jsx'
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Sidebar from '@/components/dashboard/Sidebar.jsx'
+import Header from '@/components/dashboard/Header.jsx'
+import IncomingCall from '@/components/dialogs/IncomingCall';
+
 export default function DashboardLayout() {
+  const [openDialog, setOpenDialog] = useState(true)
+
   return (
     <div className="flex min-h-screen">
       <div className="hidden md:block">
@@ -10,6 +15,7 @@ export default function DashboardLayout() {
       </div>
       <div className="flex flex-col flex-1 md:ml-[218px] lg:ml-[234px]">
         <Header />
+        {openDialog && <IncomingCall />}
         <main className="flex-1 overflow-auto p-4">
           <Outlet/>
         </main>
