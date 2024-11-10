bb2eaaab54628d4c4fe60b4df78f3f30f597ff55
Fatima Iqbal <fatimaiqbalmirza002@gmail.com>
2024-11-10 18:49:12 +0500

    Update Mobile Doctors Screen UI

--- doctor-assistance-mobile/app/(patient)/(tabs)/doctors.jsx
+++ doctor-assistance-mobile/app/(patient)/(tabs)/doctors.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import DoctorSearchBar from '@/components/dashboard/DoctorSearchBar';
 import TopDoctors from '@/components/dashboard/TopDoctors';
 import Banner from '@/components/dashboard/Banner';
+import CustomKeyboardView from '@/components/ui/CustomKeyboardView';
 
 export default function DoctorsList() {
 
   return (
-    <View className="flex-1 px-5 py-5 bg-white">
-      <DoctorSearchBar />
+    <CustomKeyboardView>
+      <View className="flex-1 px-5 py-5 bg-white">
+        <DoctorSearchBar />
 
-      <Banner imageSource={require('@/assets/images/banner2.webp')} />
+        <Banner imageSource={require('@/assets/images/banner2.webp')} />
 
-      <View>
-        <View className="flex flex-row justify-between items-center mb-3">
-          <Text className="text-lg font-semibold text-primary"> Top Medical Specialists </Text>
+        <View>
+          <View className="flex flex-row justify-between items-center mb-3">
+            <Text className="text-lg font-semibold text-primary"> Top Medical Specialists </Text>
+          </View>
+          <ScrollView>
+            <TopDoctors />
+          </ScrollView>
         </View>
-        <TopDoctors />
-      </View>
 
-      <View>
-        <View className="flex flex-row justify-between items-center mb-3">
-          <Text className="text-lg font-semibold text-primary">Nearby Doctors </Text>
-        </View>
-        <TopDoctors />
       </View>
-
-    </View>
+    </CustomKeyboardView>
   );
 }
